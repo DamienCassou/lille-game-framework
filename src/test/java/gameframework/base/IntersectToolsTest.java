faa4eb6a0a781bcd7573dade9b1fba14a4158bca
cassou <cassou@78b29cd5-96ff-41cb-9a0e-92925504f19a>
2014-07-14 15:07:49 +0000

    Clean cassebriques
    git-svn-id: https://svn-ens.fil.univ-lille1.fr/svn/COO-L3/trunk/code-source/gameframework@36 78b29cd5-96ff-41cb-9a0e-92925504f19a

--- src/test/java/gameframework/base/IntersectToolsTest.java
+++ src/test/java/gameframework/base/IntersectToolsTest.java
@@ -1,72 +1,126 @@
 package gameframework.base;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.Shape;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class IntersectToolsTest implements Movable {
 
-	private int speed;
 	private Point direction;
-	private Point position;
+	private final Point position = new Point(0, 0);
+	private final int height = 2;
+	private final int width = 3;
+	private final int speed = 4;
 
 	@Test
-	public void leftToRight() throws Exception {
-		setPosition(0, 0);
-		setDirection(1, 0);
-		setSpeed(2);
-		assertShape(0, 0, 3, 1);
+	public void gotoWest() throws Exception {
+		direction = new Point(1, 0);
+		assertShape(0, 0, width + speed, height);
 	}
 
 	@Test
-	public void rightToLeft() throws Exception {
-		setPosition(0, 0);
-		setDirection(-1, 0);
-		setSpeed(2);
-		assertShape(-2, 0, 1, 1);
+	public void gotoEast() throws Exception {
+		direction = new Point(-1, 0);
+		assertShape(-speed, 0, width, height);
 	}
 
 	@Test
-	public void upToDown() throws Exception {
-		setPosition(0, 0);
-		setDirection(0, 1);
-		setSpeed(2);
-		assertShape(0, 0, 1, 3);
+	public void gotoSouth() throws Exception {
+		direction = new Point(0, 1);
+		assertShape(0, 0, width, height + speed);
 	}
 
 	@Test
-	public void downToUp() throws Exception {
-		setPosition(0, 0);
-		setDirection(0, -1);
-		setSpeed(2);
-		assertShape(0, -2, 1, 1);
+	public void gotoNorth() throws Exception {
+		direction = new Point(0, -1);
+		assertShape(0, -speed, width, height);
+	}
+
+	@Test
+	public void gotoNorthWest() throws Exception {
+		direction = new Point(-1, -1);
+		assertShape(//
+				new Point(-speed, -speed), //
+				new Point(-speed, -speed + height), //
+				new Point(width, height));
+	}
+
+	@Test
+	public void gotoNorthEast() throws Exception {
+		direction = new Point(1, -1);
+		assertShape(//
+				new Point(speed + width, -speed), //
+				new Point(speed + width, -speed + height), //
+				new Point(0, height));
+	}
+
+	@Test
+	public void gotoSouthWest() throws Exception {
+		direction = new Point(-1, 1);
+		assertShape(//
+				new Point(-speed, speed), //
+				new Point(-speed, speed + height), //
+				new Point(width, 0));
+	}
+
+	@Test
+	public void gotoSouthEast() throws Exception {
+		direction = new Point(1, 1);
+		assertShape(//
+				new Point(speed + width, speed + height), //
+				new Point(speed + width, speed), //
+				new Point(0, 0));
+	}
+
+	@Test
+	public void gotoSomewhereElse() throws Exception {
+		// this is something like East-South-East
+		int directionX = 2;
+		int directionY = 1;
+		direction = new Point(directionX, directionY);
+		assertShape(//
+				new Point(directionX * speed + width, directionY * speed
+						+ height), //
+				new Point(directionX * speed + width, directionY * speed), //
+				new Point(0, 0));
+	}
+
+	private void assertShape(Point... points) {
+		Shape intersectShape = createIntersectionShape();
+		assertTrue(intersectShape instanceof Polygon);
+		Polygon polygon = (Polygon) intersectShape;
+		for (Point point : points) {
+			assertPolygonHasVertex(polygon, point);
+		}
+	}
+
+	private void assertPolygonHasVertex(Polygon polygon, Point point) {
+		for (int i = 0; i < polygon.npoints; i++) {
+			if (new Point(polygon.xpoints[i], polygon.ypoints[i]).equals(point))
+				return;
+		}
+		fail("Point " + point + " is not in polygon");
 	}
 
 	private void assertShape(int x1, int y1, int x2, int y2) {
-		Shape intersectShape = IntersectTools.getIntersectShape(this, new SpeedVectorDefaultImpl(direction, speed));
+		Shape intersectShape = createIntersectionShape();
 		assertTrue(intersectShape instanceof Rectangle);
 		Rectangle rectangle = (Rectangle) intersectShape;
 		assertEquals(new Point(x1, y1), rectangle.getLocation());
 		assertEquals(new Dimension(x2 - x1, y2 - y1), rectangle.getSize());
 	}
 
-	private void setSpeed(int speed) {
-		this.speed = speed;
-	}
-
-	private void setDirection(int x, int y) {
-		this.direction = new Point(x, y);
-	}
-
-	private void setPosition(int x0, int y0) {
-		this.position = new Point(x0, y0);
+	private Shape createIntersectionShape() {
+		return IntersectTools.getIntersectShape(this,
+				new SpeedVectorDefaultImpl(direction, speed));
 	}
 
 	@Override
@@ -80,6 +134,11 @@ public class IntersectToolsTest implements Movable {
 	}
 
 	@Override
+	public Rectangle getBoundingBox() {
+		return new Rectangle(position, new Dimension(width, height));
+	}
+
+	@Override
 	public void oneStepMove() {
 		fail("No need to move this movable");
 	}
@@ -87,11 +146,6 @@ public class IntersectToolsTest implements Movable {
 	@Override
 	public void setSpeedVector(SpeedVector m) {
 		fail("No need to set the speed vector");
-	}
-
-	@Override
-	public Rectangle getBoundingBox() {
-		return new Rectangle(position, new Dimension(1, 1));
 	}
 
 }
