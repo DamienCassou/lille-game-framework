690b06ad202b423a2fa087492eb8f36c2fc08cca
cassou <cassou@78b29cd5-96ff-41cb-9a0e-92925504f19a>
2014-07-15 13:27:22 +0000

    Add a class comment
    git-svn-id: https://svn-ens.fil.univ-lille1.fr/svn/COO-L3/trunk/code-source/gameframework@54 78b29cd5-96ff-41cb-9a0e-92925504f19a

--- src/main/java/gameframework/motion/blocking/MoveBlockerRulesApplierDefaultImpl.java
+++ src/main/java/gameframework/motion/blocking/MoveBlockerRulesApplierDefaultImpl.java
@@ -5,6 +5,30 @@ import gameframework.motion.Movable;
 import java.lang.reflect.Method;
 import java.util.Vector;
 
+/**
+ * Take care of special blocking rules for your game. By default, a
+ * <code>MoveBlocker</code> always blocks a <code>Movable</code>. But you can
+ * change this behavior by sub-classing
+ * <code>MoveBlockerRulesApplierDefaultImpl</code> and implementing one or more
+ * <code>moveBlockerRule</code> methods.
+ * 
+ * For example, in a typical Pacman game, a <code>moveBlockerRule</code> method
+ * could look like:
+ * 
+ * <pre>
+ * {@code
+ * public void moveBlockerRule(Ghost ghost, Wall wall) throws IllegalMoveException {
+ *   // The default case is when a ghost is active and not able to cross a
+ *   // wall. As soon as the ghost has been eaten by Pacman, the ghost becomes
+ *   // inactive and will cross walls to return to its jail in straight line. 
+ *   if (ghost.isActive()) {
+ *     throw new IllegalMoveException();
+ *   }
+ * }
+ * }
+ * </pre>
+ * 
+ */
 public class MoveBlockerRulesApplierDefaultImpl implements
 		MoveBlockerRulesApplier {
 
