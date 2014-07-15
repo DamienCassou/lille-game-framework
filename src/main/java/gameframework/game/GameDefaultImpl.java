54e98662824ee9180d56c51f841318628cd1cba7
cassou <cassou@78b29cd5-96ff-41cb-9a0e-92925504f19a>
2014-07-15 13:27:15 +0000

    Suppress 2 warnings
    git-svn-id: https://svn-ens.fil.univ-lille1.fr/svn/COO-L3/trunk/code-source/gameframework@47 78b29cd5-96ff-41cb-9a0e-92925504f19a

--- src/main/java/gameframework/game/GameDefaultImpl.java
+++ src/main/java/gameframework/game/GameDefaultImpl.java
@@ -32,7 +32,9 @@ public class GameDefaultImpl implements Game, Observer {
 	public static final int NUMBER_OF_LIVES = 1;
 
 	protected CanvasDefaultImpl defaultCanvas = null;
+	@SuppressWarnings("unchecked")
 	protected ObservableValue<Integer> score[] = new ObservableValue[MAX_NUMBER_OF_PLAYER];
+	@SuppressWarnings("unchecked")
 	protected ObservableValue<Integer> life[] = new ObservableValue[MAX_NUMBER_OF_PLAYER];
 
 	// initialized before each level
