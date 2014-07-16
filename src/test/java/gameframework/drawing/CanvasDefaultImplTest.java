e22bf626831ff055503da5bc75b0cdcd3254f5c6
cassou <cassou@78b29cd5-96ff-41cb-9a0e-92925504f19a>
2014-07-16 20:58:24 +0000

    New tests
    git-svn-id: https://svn-ens.fil.univ-lille1.fr/svn/COO-L3/trunk/code-source/gameframework@69 78b29cd5-96ff-41cb-9a0e-92925504f19a

---
+++ src/test/java/gameframework/drawing/CanvasDefaultImplTest.java
@@ -0,0 +1,53 @@
+package gameframework.drawing;
+
+import gameframework.game.GameUniverseViewPort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CanvasDefaultImplTest {
+
+	GameUniverseViewPort viewPort;
+	int paintCalled = 0;
+
+	@Before
+	public void createViewPort() {
+		viewPort = new GameUniverseViewPort() {
+
+			@Override
+			public void refresh() {
+			}
+
+			@Override
+			public void paint() {
+				paintCalled++;
+			}
+		};
+	}
+
+	@Test
+	public void paintDelegatesToViewPort() {
+		CanvasDefaultImpl canvas = new CanvasDefaultImpl();
+		canvas.setDrawingGameBoard(viewPort);
+		canvas.paint(new MockGraphics());
+		assertEquals(1,paintCalled);
+	}
+	
+	@Test
+	public void repaintDelegatesToViewPort() {
+		CanvasDefaultImpl canvas = new CanvasDefaultImpl();
+		canvas.setDrawingGameBoard(viewPort);
+		canvas.repaint();
+		assertEquals(1,paintCalled);
+	}
+
+	@Test
+	public void painWithNoViewPort() throws Exception {
+		CanvasDefaultImpl canvas = new CanvasDefaultImpl();
+		canvas.paint(new MockGraphics());
+		assertEquals(0,paintCalled);
+	}
+	
+}
