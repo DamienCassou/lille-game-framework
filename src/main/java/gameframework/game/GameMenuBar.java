aa36f4fe9b4c67b25838c3fb58b992366793be43
cassou <cassou@78b29cd5-96ff-41cb-9a0e-92925504f19a>
2014-07-19 09:18:29 +0000

    Dedicated classes for the menubar and statusbar
    git-svn-id: https://svn-ens.fil.univ-lille1.fr/svn/COO-L3/trunk/code-source/gameframework@77 78b29cd5-96ff-41cb-9a0e-92925504f19a

---
+++ src/main/java/gameframework/game/GameMenuBar.java
@@ -0,0 +1,29 @@
+package gameframework.game;
+
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class GameMenuBar {
+
+	public MenuBar getComponent() {
+		MenuBar menuBar = new MenuBar();
+		Menu file = new Menu("file");
+		MenuItem quit = new MenuItem("quit");
+		menuBar.add(file);
+
+		quit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+		file.add(quit);
+		
+		return menuBar;
+	}
+	
+}
---
+++ src/main/java/gameframework/game/GameStatusBar.java
@@ -0,0 +1,55 @@
+package gameframework.game;
+
+import gameframework.base.ObservableValue;
+
+import java.awt.Container;
+import java.awt.GridBagLayout;
+import java.awt.Label;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+public class GameStatusBar implements Observer {
+
+	protected Label lifeText, scoreText;
+	protected Label lifeValue, scoreValue;
+
+	protected final ObservableValue<Integer> score;
+	protected final ObservableValue<Integer> life;
+
+	public GameStatusBar(ObservableValue<Integer> score,
+			ObservableValue<Integer> life) {
+		this.score = score;
+		this.life = life;
+		score.addObserver(this);
+		life.addObserver(this);
+	}
+
+	public Container getContainer() {
+		JPanel c = new JPanel();
+		GridBagLayout layout = new GridBagLayout();
+		c.setLayout(layout);
+		lifeText = new Label("Lives:");
+		scoreText = new Label("Score:");
+		lifeValue = new Label();
+		scoreValue = new Label();
+		c.add(lifeText);
+		c.add(lifeValue);
+		c.add(scoreText);
+		c.add(scoreValue);
+		update();
+		return c;
+	}
+
+	public void update() {
+		lifeValue.setText(Integer.toString(life.getValue()));
+		scoreValue.setText(Integer.toString(score.getValue()));
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		update();
+	}
+
+}
