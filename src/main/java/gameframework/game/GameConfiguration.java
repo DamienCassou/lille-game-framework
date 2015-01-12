dc3efeb707bb9cc5bcf005268cb0e92fd4a65cc9
Delvallet Lucas <delvallet@a14p15.fil.univ-lille1.fr>
2015-01-12 15:43:19 +0100

    Added Class Diagramm
    Adding backgroungImage method with parameter.
    
    In GameUniverseViewPort the method backgroundImage has no parameter, so
    I added a method of the same name with one parameter so that one can
    choose the path for the image.
    
    Added GameConfiguration constructor with parameterisable size.

--- src/main/java/gameframework/game/GameConfiguration.java
+++ src/main/java/gameframework/game/GameConfiguration.java
@@ -12,21 +12,68 @@ import gameframework.motion.overlapping.OverlapRulesApplier;
 import gameframework.motion.overlapping.OverlapRulesApplierDefaultImpl;
 
 public class GameConfiguration {
+	
+	protected final int nbRows;
+	protected final int nbColumns;
+	protected final int spriteSize;
+	protected final int nbLives;
 
+	private static final int DEFAULT_NBROWS = 31;
+	private static final int DEFAULT_NBCOL = 28;
+	private static final int DEFAULT_SPRITESIZE = 16;
+	private static final int DEFAULT_NBLIVES = 2;
+
+	// CONSTRUCTORS
+	
+	/**
+	 * Constructor with parameterisable size. Create a new GameConfiguration with the specified
+	 * parameters. If 0 is specified as one of the parameter, the constructor uses the default
+	 * value for this parameter. 
+	 * @param nbRows The number of rows of the window (default value: {@value #DEFAULT_NBROWS}).
+	 * @param nbColumns The number of columns of the window (default value: {@value #DEFAULT_NBCOL}). 
+	 * @param spriteSize The size of the sprites displayed (default value: {@value #DEFAULT_SPRITESIZE}).
+	 * @param nbLives The number of lives of the player (default value: {@value #DEFAULT_NBLIVES}).
+	 */
+	public GameConfiguration(int nbRows, int nbColumns, int spriteSize,
+			int nbLives) {
+
+		this.nbRows = nbRows == 0
+					? DEFAULT_NBROWS
+					: nbRows;
+
+		this.nbColumns = nbColumns == 0
+					 ? DEFAULT_NBCOL
+					 : nbColumns;
+
+		this.spriteSize = spriteSize == 0
+						? DEFAULT_SPRITESIZE
+						: spriteSize;
+		
+		this.nbLives = nbLives == 0
+					 ? DEFAULT_NBLIVES
+					 : nbLives;
+	}
+
+	public GameConfiguration() {
+		this(DEFAULT_NBROWS, DEFAULT_NBCOL, DEFAULT_SPRITESIZE, DEFAULT_NBLIVES);
+	}
+
+	// METHODS
+	
 	public int getNbRows() {
-		return 31;
+		return nbRows;
 	}
 
 	public int getNbColumns() {
-		return 28;
+		return nbColumns;
 	}
 
 	public int getSpriteSize() {
-		return 16;
+		return spriteSize;
 	}
 
 	public int getDefaultNbLives() {
-		return 2;
+		return nbLives;
 	}
 
 	public GameCanvas createCanvas() {
@@ -40,7 +87,7 @@ public class GameConfiguration {
 	public MoveBlockerChecker createMoveBlockerChecker() {
 		return new MoveBlockerCheckerDefaultImpl();
 	}
-	
+
 	public OverlapRulesApplier createOverlapRulesApplier() {
 		return new OverlapRulesApplierDefaultImpl();
 	}
@@ -54,3 +101,4 @@ public class GameConfiguration {
 	}
 
 }
+
