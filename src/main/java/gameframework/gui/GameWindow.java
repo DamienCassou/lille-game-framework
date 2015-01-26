45b4753e6406e6d0dd2522a7680692243adbb727
juliensoret <julien@soret.in>
2015-01-26 10:22:23 +0100

    GameData in parameter, includes Configuration, Score, Life.

--- src/main/java/gameframework/gui/GameWindow.java
+++ src/main/java/gameframework/gui/GameWindow.java
@@ -3,6 +3,7 @@ package gameframework.gui;
 import gameframework.base.ObservableValue;
 import gameframework.drawing.GameCanvas;
 import gameframework.game.GameConfiguration;
+import gameframework.game.GameData;
 
 import java.awt.BorderLayout;
 import java.awt.Frame;
@@ -24,6 +25,12 @@ public class GameWindow {
 		this("Default Game", gameCanvas, configuration,
 				new GameStatusBarElement<>("Score:", score),
 				new GameStatusBarElement<>("Life:", life));
+	}
+	
+	public GameWindow(String gameName, GameCanvas gameCanvas, GameData data) {
+		this(gameName, gameCanvas, data.getConfiguration(),
+				new GameStatusBarElement<>("Score:", data.getScore()),
+				new GameStatusBarElement<>("Life:", data.getLife()));
 	}
 
 	public GameWindow(String gameName, GameCanvas gameCanvas,
