1a0a529e0cce57c634162e159c95d08bfe34eff8
Orandin <simon.delberghe@etudiant.univ-lille1.fr>
2016-01-09 18:32:16 +0100

    Add mocks

---
+++ src/test/java/gameframework/game/mocks/GameDataMock.java
@@ -0,0 +1,10 @@
+package gameframework.game.mocks;
+
+import gameframework.game.GameData;
+
+public class GameDataMock extends GameData {
+
+	public GameDataMock() {
+		super(new GameConfigurationMock());
+	}
+}
---
+++ src/test/java/gameframework/game/mocks/GameEntityMock.java
@@ -0,0 +1,13 @@
+package gameframework.game.mocks;
+
+import java.awt.Graphics;
+
+import gameframework.game.GameEntity;
+
+public class GameEntityMock implements GameEntity {
+
+	@Override
+	public void draw(Graphics g) {
+			
+	}
+}
