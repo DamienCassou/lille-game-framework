cc1704edf0f8e1ba378117f2639fb2676f8477c7
Orandin <simon.delberghe@etudiant.univ-lille1.fr>
2016-01-09 18:41:03 +0100

    Add getters

--- src/main/java/gameframework/motion/blocking/MoveBlockerChecker.java
+++ src/main/java/gameframework/motion/blocking/MoveBlockerChecker.java
@@ -1,5 +1,7 @@
 package gameframework.motion.blocking;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import gameframework.motion.Movable;
 import gameframework.motion.SpeedVector;
 
@@ -8,8 +10,9 @@ public interface MoveBlockerChecker {
 
 	public void removeMoveBlocker(MoveBlocker blocker);
 
+	public ConcurrentLinkedQueue<MoveBlocker> getMoveBlockers();
+
 	public void setMoveBlockerRules(MoveBlockerRulesApplier moveBlockerRules);
 
-	public boolean moveValidation(Movable movable,
-			SpeedVector requestedSpeedVector);
+	public boolean moveValidation(Movable movable, SpeedVector requestedSpeedVector);
 }
