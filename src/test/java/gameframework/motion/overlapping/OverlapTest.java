8abe384091cb105e8f3da25d88b6b2287de94e4b
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-01-15 17:19:46 +0100

    adding @before in the test class

--- src/test/java/gameframework/motion/overlapping/OverlapTest.java
+++ src/test/java/gameframework/motion/overlapping/OverlapTest.java
@@ -6,31 +6,34 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class OverlapTest {
 
+	Overlappable overlappable1;
+	Overlappable overlappable2; 
+	Overlap overlap;
+
+	@Before
+	public void create(){
+		 overlappable1 = new Overlappable1();
+		 overlappable2 = new Overlappable1();
+		 overlap = new Overlap(overlappable1, overlappable2);
+	}
+	
 	@Test
 	public void getOverlappable1Test() {
-		Overlappable overlappable1 =  new Overlappable1();
-		Overlappable overlappable2 =  new Overlappable1();
-		Overlap overlap = new Overlap(overlappable1, overlappable2);
 		assertSame(overlappable1, overlap.getOverlappable1());		
 	}
 	
 	@Test
 	public void getOverlappable2Test() {
-		Overlappable overlappable1 =  new Overlappable1();
-		Overlappable overlappable2 =  new Overlappable1();
-		Overlap overlap = new Overlap(overlappable1, overlappable2);
 		assertSame(overlappable2, overlap.getOverlappable2());		
 	}
 	
 	@Test
 	public void getOverlappablesTest(){
-			Overlappable overlappable1 =  new Overlappable1();
-			Overlappable overlappable2 =  new Overlappable1();
-			Overlap overlap = new Overlap(overlappable1, overlappable2);
 			Set<Overlappable> set = new HashSet<Overlappable>(Arrays.asList(overlappable1,
 					overlappable2));
 			assertEquals(set, overlap.getOverlappables());		
