db83f8bf50bbfff69a260aaedf2342c38c40d795
Jonathan Lecointe <jonathan.lecointe@gmail.com>
2016-01-15 16:42:50 +0100

    Allow to remove the Game status bar in GameWindow + comments

--- src/main/java/gameframework/gui/GameWindow.java
+++ src/main/java/gameframework/gui/GameWindow.java
@@ -1,5 +1,6 @@
 package gameframework.gui;
 
+import gameframework.base.ObservableValue;
 import gameframework.drawing.GameCanvas;
 import gameframework.game.GameConfiguration;
 import gameframework.game.GameData;
@@ -13,17 +14,67 @@ import java.awt.Component;
 
 public class GameWindow {
 
-	protected final Frame frame;
+	protected Frame frame;
 	protected GameCanvas gameCanvas;
 	protected final GameStatusBar statusBar = new GameStatusBar();
+
+
+	@Deprecated
+	public GameWindow(GameCanvas gameCanvas, GameConfiguration configuration,
+			final ObservableValue<Integer> score,
+			final ObservableValue<Integer> life) {
+		this("Default Game", gameCanvas, configuration,
+				new GameStatusBarElement<>("Score:", score),
+				new GameStatusBarElement<>("Life:", life));
+	}
 	
+	/**
+	 * @param gameName the name of the window
+	 * @param gameCanvas the canvas (View in MVC design pattern)
+	 * @param data the game data (Model in MVC design pattern)
+	 */
 	public GameWindow(String gameName, GameCanvas gameCanvas, GameData data) {
 		this(gameName, gameCanvas, data.getConfiguration(),
 				new GameStatusBarElement<>("Score:", data.getScore()),
 				new GameStatusBarElement<>("Life:", data.getLife()));
 	}
+	
+	/**
+	 * @param gameName the name of the window
+	 * @param gameCanvas the canvas (View in MVC design pattern)
+	 * @param data the game data (Model in MVC design pattern)
+	 * @param enableStatusBar true or false to enable or disable Game Status Bar
+	 */
+	public GameWindow(String gameName, GameCanvas gameCanvas, GameData data, boolean enableStatusBar) {
+		if (enableStatusBar) {
+			init(gameName, gameCanvas, data.getConfiguration(),
+					new GameStatusBarElement<>("Score:", data.getScore()),
+					new GameStatusBarElement<>("Life:", data.getLife()));
+		} else {
+			init(gameName, gameCanvas, data.getConfiguration());
+		}
+	}
 
+	/**
+	 * @param gameName the name of the window
+	 * @param gameCanvas the canvas (View in MVC design pattern)
+	 * @param configuration the game configuration
+	 * @param elementsStatusBar list of element(s) for the status bar
+	 */
 	public GameWindow(String gameName, GameCanvas gameCanvas,
+			GameConfiguration configuration,
+			GameStatusBarElement<?>... elementsStatusBar) {
+		init(gameName, gameCanvas, configuration, elementsStatusBar);
+	}
+	
+	/**
+	 * Initialize the game window
+	 * @param gameName the name of the window
+	 * @param gameCanvas the canvas (View in MVC design pattern)
+	 * @param configuration the game configuration
+	 * @param elementsStatusBar list of element(s) for the status bar
+	 */
+	private void init(String gameName, GameCanvas gameCanvas,
 			GameConfiguration configuration,
 			GameStatusBarElement<?>... elementsStatusBar) {
 		if (gameCanvas == null) {
@@ -32,11 +83,14 @@ public class GameWindow {
 		this.statusBar.addAll(elementsStatusBar);
 		this.frame = new Frame(gameName);
 		this.gameCanvas = gameCanvas;
-		this.gameCanvas.setSize(//
-				configuration.getSpriteSize() * configuration.getNbColumns(), //
+		this.gameCanvas.setSize(
+				configuration.getSpriteSize() * configuration.getNbColumns(), 
 				configuration.getSpriteSize() * configuration.getNbRows());
 	}
 
+	/**
+	 * Create the main frame of the game.
+	 */
 	public void createGUI() {
 		frame.dispose();
 		frame.setMenuBar(new GameMenuBar().getComponent());
@@ -53,6 +107,10 @@ public class GameWindow {
 		});
 	}
 
+	/**
+	 * @param comp
+	 * @param layout
+	 */
 	public void add(Component comp, String layout) {
 		this.frame.add(comp, layout);
 		this.frame.pack();
