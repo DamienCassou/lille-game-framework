31a896ab6b43a3adff64f888212e6876221eb740
Joël Troch <joel.troch62@gmail.com>
2017-01-06 17:15:25 +0100

    Add documentation for ObservableValue

--- src/main/java/gameframework/base/ObservableValue.java
+++ src/main/java/gameframework/base/ObservableValue.java
@@ -2,13 +2,31 @@ package gameframework.base;
 
 import java.util.Observable;
 
+/**
+ * Class that represent an observable value.
+ * Observable values are ideal to "trigger events"
+ * upon value changes.
+ */
 public class ObservableValue<T> extends Observable {
+
+	/** The value itself. */
 	private T value;
 
+	/**
+	 * Create a new observable value with an initial value.
+	 * @param initial Initial value to use.
+	 */
 	public ObservableValue(T initial) {
 		setValue(initial);
 	}
 
+	/**
+	 * Set a new value for this observable value.
+	 * This will call "setChanged()" and "notifyObservers()" methods.
+	 * If the new value is identical to the actual one, nothing will
+	 * happen.
+	 * @param newValue The new value to set.
+	 */
 	public void setValue(T newValue) {
 		if (value != newValue) {
 			this.value = newValue;
@@ -17,6 +35,10 @@ public class ObservableValue<T> extends Observable {
 		}
 	}
 
+	/**
+	 * Returns the value of this observed value.
+	 * @return The value of this observed value.
+	 */
 	public T getValue() {
 		return value;
 	}
