db51bf07199bbccb92cb1b9b86ce91fbcac00be0
Ladent Thibault <ladent@a14p5.fil.univ-lille1.fr>
2017-01-20 15:52:10 +0100

    add doc MoveStrategy + MoveStrategyStraightLine

--- src/main/java/gameframework/motion/MoveStrategy.java
+++ src/main/java/gameframework/motion/MoveStrategy.java
@@ -1,6 +1,14 @@
 package gameframework.motion;
-
+/**
+ * Define how to build a {@link SpeedVector} with a particular strategy
+ * 
+ *
+ */
 public interface MoveStrategy {
+	/**
+	 * Build a specific {@link SpeedVector} depending of the MoveStrategy used
+	 * @return the speedvector built
+	 */
 	public SpeedVector getSpeedVector();
 	
 	public int getSpeed();
